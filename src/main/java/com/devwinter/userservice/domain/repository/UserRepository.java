6704d26403646056b3c9381d433e891deb1c63f1
donghun93 <alswn4516@gmail.com>
2023-01-26 16:52:22 +0900

    Rename: 회원 도메인 이름 변경 및 패키지 이동
    domain 패키지 안에 entity 패키지로 이동 시켰다.

--- src/main/java/com/devwinter/userservice/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devwinter.userservice.domain.repository;
-
-import com.devwinter.userservice.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
